8fa447976f99c5444c8d1631fa9d96e4bc0f1ca0
fedegiovannetti <giovannettipsi@gmail.com>
2025-03-06 10:21:21 -0300

    Version 1.0

---
+++ repo_upload.R
@@ -0,0 +1,15 @@
+library(osfr)
+library(gert)
+
+git_add(".")
+git 
+
+# osf_auth("jK8aZe7ym3KBv7AwyZdLrBHDB8jiOCwh0vX3cIahaxiptPMZ7oNZ6EvOAWOUeOFZi8DXfx")
+# 
+# # my_project <- osf_create_project(
+# #   title = "Estudio Rose"
+# # )
+# 
+# ROSE <- osf_retrieve_node("https://osf.io/43w5m/")
+# 
+# r_files <- osf_upload(ROSE, path = list.files(),conflicts = "overwrite")
